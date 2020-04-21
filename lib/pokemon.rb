86d9c75545f6385d5522efc9815ba31ffcb509a8
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-21 18:39:07 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db 
+  
+  
+  def initialize(id:,name:,type:,db:)
+    @id=id
+    @name=name
+    @type=type
+    @db=db
+  end 
+  
+  
+  def self.save(name,type,db)
+    sql="INSERT INTO pokemon (name,type) VALUES (?,?)"
+    DB[:conn].execute(sql,[name,type])
+    
+  end 
+  
+    
+    
+    
+  
 end
